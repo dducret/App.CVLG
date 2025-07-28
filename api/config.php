c686bfde36a4ef78b966936bfd2ced9926aa78fa
Denis Ducret <denis.ducret@sdic.ch>
2025-07-28 20:06:03 +0200

    Create config.php

---
+++ api/config.php
@@ -0,0 +1,11 @@
+<?php
+$dbFile = __DIR__ . '/../database.sqlite';
+$new = !file_exists($dbFile);
+$pdo = new PDO('sqlite:' . $dbFile);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+if ($new) {
+    $schema = file_get_contents(__DIR__ . '/../Documentation/appcvlg.db.sql');
+    $pdo->exec($schema);
+}
+?>
