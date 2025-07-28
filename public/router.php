288e52c634a7c20360a1ce6381c4749a0d57f41b
Denis Ducret <denis.ducret@sdic.ch>
2025-07-28 22:46:40 +0200

    Delete public/router.php

--- public/router.php
+++
@@ -1,9 +0,0 @@
-<?php
-// router.php
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-if (str_starts_with($uri, '/api')) {
-    // forward /api requests to the API entry point
-    include __DIR__ . '/../api/index.php';
-} else {
-    return false; // let PHP’s built-in server handle static files
-}
